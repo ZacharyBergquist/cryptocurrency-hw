7248773e4eec7cd495931012f071d34536e5c463
root <brandoncterryy@gmail.com>
2020-02-09 19:50:53 -0500

    surface test

--- test_script.py
+++ test_script.py
@@ -4,3 +4,4 @@ raw_sig = pk.ecdsa_sign(message, digest=blake2b_32)
 sig = pk.ecdsa_serialize_compact(raw_sig)
 print("secp lib sig: " + base58_encode(sig, b"spsig").decode())
 
+#yuhhhhh lets see if this works
